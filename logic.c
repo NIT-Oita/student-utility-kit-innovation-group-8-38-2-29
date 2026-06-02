1cb702f93e331247080312b07bb0033005fa48d0
s2502 <k8619923086@gmail.com>
2026-06-02 12:01:58 +0900

    logic.c修正3

--- logic.c
+++ logic.c
@@ -5,11 +5,13 @@
 #include "logic.h"
 
 int i;
-int j;
+int j=1;
+int r;
 
 void INPUT_MODE(void){
     Card cheet[128];
     int judge;
+    while(getchar() != '\n');
 
     for(i=0;i<128;i++){
         printf("単語を入力\n");
@@ -43,16 +45,16 @@ void QUIZ_MODE(void){
     char ANSWER[256];
 
     while(1 != 0){
-        i=rand()%128;
+        r=rand()%i;
 
-        printf("%d問目\n%255s",j,cheet[i].word);
+        printf("%d問目\n%255s",j,cheet[r].word);
         printf("答えを入力:");
         fgets(ANSWER, sizeof(ANSWER), stdin);
-        if(strcmp(ANSWER,cheet[i].answer) == 0){
+        if(strcmp(ANSWER,cheet[r].answer) == 0){
             printf("正解！\n");
         }else{
             printf("不正解...\n");
-            printf("答え: %255s\n",cheet[i].answer);
+            printf("答え: %255s\n",cheet[r].answer);
         }
 
         printf("入力モードを終了しますか？\n「はい」なら「1」,「いいえ」なら(それ以外)を入力:");
