5423fc12ede353cee79574626f1f10cb119d6a2e
s25300616 <s2530@oita.kosen-ac.jp>
2026-06-03 15:12:12 +0900

    storage

--- storage.c
+++ storage.c
@@ -8,7 +8,7 @@ char line[256];
 fgets(line, sizeof line, fp);   /* ヘッダ行を読み飛ばす */
 int n = 0;
 while (n < max &&
-fscanf(fp, "%,%\n",
+fscanf(fp, "%255[^\n] %255[^\n]",
 list[n].word,
 list[n].answer) == 2) {
 n++;
