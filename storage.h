c2d09cbd0af1e3a58542be221ba040e307a45fad
s25300616 <s2530@oita.kosen-ac.jp>
2026-06-21 15:25:02 +0900

    a

--- storage.h
+++ storage.h
@@ -1,14 +1,8 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
-<<<<<<< HEAD
-#include "logic.h" /* . */
-int load_tasks(const char *path,
-Task *list, int max);
-=======
-#include "logic.h"
+#include "logic.h" 
 int load_tasks(const char *path,
 Card *list, int max);
->>>>>>> 11cc3a0 (DATA)
 
 #endif
